24c6deaee8ad142b50702d970c57b4899b10c69a
Akash Antony J <akashantony224@gmail.com>
2025-06-22 20:33:11 +0530

    Add files via upload
    Design principles and pattern

---
+++ Week1_DesignPrinciplesAndPatterns/FactoryMethodPattern/code/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+interface IDocument
+{
+    void create();
+}
+
+abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+class WordDocument : IDocument
+{
+    public void create()
+    {
+        Console.WriteLine("Created Word Document...");
+    }
+}
+
+class PdfDocument : IDocument
+{
+    public void create()
+    {
+        Console.WriteLine("Created PDF Document...");
+    }
+}
+
+class ExcelDocument : IDocument
+{
+    public void create()
+    {
+        Console.WriteLine("Created Excel Document...");
+    }
+}
+
+class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordFactory();
+        IDocument word = wordFactory.CreateDocument();
+        word.create();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        IDocument pdf = pdfFactory.CreateDocument();
+        pdf.create();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        IDocument excel = excelFactory.CreateDocument();
+        excel.create();
+    }
+}
---
+++ Week1_DesignPrinciplesAndPatterns/SingletonPattern/code/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Logger
+{
+    private static Logger? instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger Initialized");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG]: {message}");
+    }
+}
+
+class SingletonPattern
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("Akash started the app.");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("Akash clicked something.");
+
+        Console.WriteLine($"Is logger1 same as logger2? {logger1 == logger2}");
+    }
+}
